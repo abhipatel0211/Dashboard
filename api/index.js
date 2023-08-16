49ae69c6f07a52971bedb0978c1ad29ebc353369
abhipatel0211 <abhiptl0123@gmail.com>
2023-08-17 01:32:23 +0530

    final commit

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // Replace with the URL of your frontend application
+    origin: `${process.env.CLIENT_URL}`, // Replace with the URL of your frontend application
     credentials: true, // Set the "Access-Control-Allow-Credentials" header to "true"
   })
 );
